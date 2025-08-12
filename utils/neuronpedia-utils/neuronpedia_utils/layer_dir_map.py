80a1746e0cd5daa1431b42f198672b093202a134
Lan Tian <77877999+lt-0123@users.noreply.github.com>
2025-08-12 14:17:46 +0800

    Create layer_dir_map.py

---
+++ utils/neuronpedia-utils/neuronpedia_utils/layer_dir_map.py
@@ -0,0 +1,38 @@
+layer_dir_map = {
+    1: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.1",
+    5: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.5",
+    9: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.9",
+    13: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.13",
+    17: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.17",
+    21: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.21",
+    25: "sparsify_layers_1_5_9_13_17_21_25_gsml4/layers.25",
+    2: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.2",
+    6: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.6",
+    10: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.10",
+    14: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.14",
+    18: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.18",
+    2: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.22",
+    26: "sparsify_layers_2_6_10_14_18_22_26_gsml4/layers.26",
+    3: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.3",
+    7: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.7",
+    1: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.11",
+    15: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.15",
+    19: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.19",
+    23: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.23",
+    27: "sparsify_layers_3_7_11_15_19_23_27_gsml4/layers.27",
+    0: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.0",
+    4: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.4",
+    8: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.8",
+    12: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.12",
+    16: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.16",
+    20: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.20",
+    24: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.24",
+    28: "sparsify_llama38b_latents32768_bs64_wandb_gsml4/layers.28",
+    29: "sparsify_layers_29_30_31_gsml4/layers.29",
+    30: "sparsify_layers_29_30_31_gsml4/layers.30",
+    31: "sparsify_layers_29_30_31_gsml4/layers.31"
+}
+
+model_map = {
+    "Llama-3-8B": "/mnt/xuekaiwen/models/models--meta-llama--Meta-Llama-3-8B/snapshots/8cde5ca8380496c9a6cc7ef3a8b46a0372a1d920"
+}
